beda029dbc7970f57bc1c9078e9c8b84422ed77b
aditya2604 <aditya26042005@gmail.com>
2022-09-17 05:30:38 -0700

    add performance testing script

---
+++ chopper-test.py
@@ -0,0 +1,96 @@
+import hatchet as ht
+from hatchet.util import timer
+import pandas as pd
+import os
+
+
+def main():
+    num_processes = [64, 128, 256, 512]
+    num_runs = 5
+    directory = "quicksilver-only-time"
+
+    # average time for each process count
+    file_read_times = []
+    flat_profile_times = []
+    load_imbalance_times = []
+    hot_path_times = []
+
+    for path in os.listdir(directory):
+        # timers
+        file_read_timer = timer.Timer()
+        flat_profile_timer = timer.Timer()
+        load_imbalance_timer = timer.Timer()
+        hot_path_timer = timer.Timer()
+
+        # total times
+        file_read_tt = 0
+        flat_profile_tt = 0
+        load_imbalance_tt = 0
+        hot_path_tt = 0
+
+        for run in range(num_runs):
+            # file read
+            file_read_timer.start_phase(f"file read {run}")
+            # gf = ht.GraphFrame.from_hpctoolkit(directory + "/" + path)
+            gf = ht.GraphFrame.from_caliper("hatchet/tests/data/caliper-lulesh-json/lulesh-annotation-profile.json")
+            file_read_timer.end_phase()
+            file_read_tt += list(file_read_timer._times.values())[run].total_seconds()
+
+            # flat profile
+            flat_profile_timer.start_phase(f"flat_profile {run}")
+            gf.flat_profile()
+            flat_profile_timer.end_phase()
+            flat_profile_tt += list(flat_profile_timer._times.values())[
+                run
+            ].total_seconds()
+
+            # load_imbalance
+            load_imbalance_timer.start_phase(f"load_imbalance {run}")
+            gf.load_imbalance("time")
+            load_imbalance_timer.end_phase()
+            load_imbalance_tt += list(load_imbalance_timer._times.values())[
+                run
+            ].total_seconds()
+
+            # hot_path
+            hot_path_timer.start_phase(f"hot_path {run}")
+            gf.hot_path()
+            hot_path_timer.end_phase()
+            hot_path_tt += list(hot_path_timer._times.values())[run].total_seconds()
+
+        file_read_times.append(file_read_tt / num_runs)
+        flat_profile_times.append(flat_profile_tt / num_runs)
+        load_imbalance_times.append(load_imbalance_tt / num_runs)
+        hot_path_times.append(hot_path_tt / num_runs)
+
+    functions = [
+        "file read",
+        "flat_profile",
+        "load_imbalance",
+        "hot_path",
+    ]
+    times = [
+        file_read_times,
+        flat_profile_times,
+        load_imbalance_times,
+        hot_path_times,
+    ]
+    dataframes = []
+
+    for i in range(len(functions)):
+        data = {
+            "num_processes": num_processes,
+            "function": [functions[i]] * len(num_processes),
+            "time": times[i],
+        }
+        dataframes.append(pd.DataFrame(data))
+
+    df_all = pd.concat(dataframes)
+    df_pivot = df_all.pivot(index="num_processes", columns="function", values="time")
+
+    print(df_pivot)
+    print(df_pivot.loc[:, :].plot.line(figsize=(10, 7)))
+
+
+if __name__ == "__main__":
+    main()
