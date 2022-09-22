fa418ff68aa3257c68fa1b987b48899f8dc5aff1
aditya2604 <aditya26042005@gmail.com>
2022-09-22 02:15:54 -0700

    add multi-graphframe functions tests

--- chopper_test.py
+++ chopper_test.py
@@ -27,22 +27,40 @@ def main():
     # ---------------------------------------------------------------------------------
     json_data = init_json("data.json")
     path_to_directory = "../hatchet-data/quicksilver-only-time"
-    # data identifier must be present in file/folder name
-    data_identifier = "hpctoolkit"
     reader_function = ht.GraphFrame.from_hpctoolkit
     metric = "REALTIME (sec) (I)"
     num_runs = 5
     num_processes = [64, 128, 256, 512]
+    # single-graphframe functions
     functions = [
         "file read",
         "to_callgraph",
         "load_imbalance",
         "hot_path",
     ]
+
+    # replace keys with dataset names to appear on x-axis of plot
+    paths_to_directories = {
+        "quicksilver": {
+            "path": "../hatchet-data/quicksilver-only-time",
+            "num_processes": [64, 128, 256, 512],
+            "metric": "REALTIME (sec) (I)",
+        },
+        "LULESH": {
+            "path": "../hatchet-data/caliper-lulesh-json",
+            "num_processes": [1, 8, 27, 64, 125, 216, 343, 512],
+            "metric": "time",
+        },
+    }
+    # multi-graphframe functions
+    multi_gf_functions = [
+        "construct_from",
+        "multirun_analysis",
+        "calculate_speedup_efficiency",
+    ]
     # ---------------------------------------------------------------------------------
 
     directory = natsorted(os.listdir(path_to_directory))
-    directory = [path for path in directory if data_identifier in path]
     data = {}
     for function in functions:
         data[function] = dict(
@@ -51,8 +69,19 @@ def main():
                 "total_time": 0,
                 # list of average times for each process count
                 "times_list": [],
-                # number rows in dataframes for each dataset
-                "Rows in Dataframe": [],
+                # number of rows in dataframes for each dataset
+                "num_rows": 0,
+            }
+        )
+    for function in multi_gf_functions:
+        data[function] = dict(
+            {
+                "timer": timer.Timer(),
+                "total_time": 0,
+                # list of average times for each process count
+                "times_list": [],
+                # name of dataset that function is called on
+                "dataset_name": "",
             }
         )
     dataframes = []
@@ -100,13 +129,71 @@ def main():
                 )[run].total_seconds()
 
         for function in functions:
-            data[function]["Rows in Dataframe"].append(gf.dataframe.shape[0])
+            data[function]["num_rows"] = gf.dataframe.shape[0]
+            data[function]["times_list"].append(data[function]["total_time"] / num_runs)
+
+    for dataset in paths_to_directories.keys():
+        reset_time()
+        for run in range(num_runs):
+            # construct_from
+            if "construct_from" in multi_gf_functions:
+                directory = natsorted(os.listdir(path_to_directory[dataset]["path"]))
+                data["construct_from"]["timer"].start_phase(f"construct_from {run}")
+                gfs = ht.GraphFrame.construct_from(directory)
+                data["construct_from"]["timer"].end_phase()
+                data["construct_from"]["total_time"] += list(
+                    data["construct_from"]["timer"]._times.values()
+                )[run].total_seconds()
+                for gf in gfs:
+                    gf.default_metric = paths_to_directories[dataset]["metric"]
+                    # check that number of given process counts is equal to number of graphframes generated
+                    assert len(gfs) == len(
+                        paths_to_directories[dataset]["num_processes"]
+                    ), f"number of process counts does not match number of graphframes for {dataset}"
+                    gf.update_metadata(
+                        num_processes=paths_to_directories[dataset]["num_processes"][
+                            gfs.index(gf)
+                        ]
+                    )
+
+            # multirun_analysis
+            if "multirun_analysis" in multi_gf_functions:
+                data["multirun_analysis"]["timer"].start_phase(
+                    f"multirun_analysis {run}"
+                )
+                ht.GraphFrame.multirun_analysis(
+                    graphframes=gfs,
+                    pivot_index="num_processes",
+                    columns="name",
+                    metric=paths_to_directories[dataset]["metric"],
+                    threshold=0,
+                )
+                data["multirun_analysis"]["timer"].end_phase()
+                data["multirun_analysis"]["total_time"] += list(
+                    data["multirun_analysis"]["timer"]._times.values()
+                )[run].total_seconds()
+
+            # calculate_speedup_efficiency
+            if "calculate_speedup_efficiency" in multi_gf_functions:
+                data["calculate_speedup_efficiency"]["timer"].start_phase(
+                    f"calculate_speedup_efficiency {run}"
+                )
+                ht.GraphFrame.calculate_speedup_efficiency(
+                    graphframes=gfs, metric=paths_to_directories[dataset]["metric"]
+                )
+                data["calculate_speedup_efficiency"]["timer"].end_phase()
+                data["calculate_speedup_efficiency"]["total_time"] += list(
+                    data["calculate_speedup_efficiency"]["timer"]._times.values()
+                )[run].total_seconds()
+
+        for function in multi_gf_functions:
+            data[function]["dataset_name"] = dataset
             data[function]["times_list"].append(data[function]["total_time"] / num_runs)
 
     for function in functions:
         d = {
             "num_processes": num_processes,
-            "Rows in Dataframe": data[function]["Rows in Dataframe"],
+            "Rows in Dataframe": [data[function]["num_rows"]] * len(num_processes),
             "function": [function] * len(num_processes),
             "time": data[function]["times_list"],
         }
@@ -114,13 +201,39 @@ def main():
         with open("data.json", "w") as f:
             json.dump(json_data, f)
 
-    for func_data in json_data.values():
-        dataframes.append(pd.DataFrame(func_data))
+    # single-graphframe functions plot
+    for function, data in json_data.items():
+        if function in functions:
+            dataframes.append(pd.DataFrame(data))
 
     df_all = pd.concat(dataframes)
     df_pivot = df_all.pivot_table(
         index="Rows in Dataframe", columns="function", values="time"
     )
+    df_plot = df_pivot.loc[:, :].plot.line(figsize=(10, 7))
+
+    print(df_pivot)
+    print(df_plot)
+
+    dataframes.clear()
+
+    # multi-graphframe functions plot
+    for function in multi_gf_functions:
+        d = {
+            "dataset": [data[function]["dataset_name"]] * len(paths_to_directories),
+            "function": [function] * len(paths_to_directories),
+            "time": data[function]["times_list"],
+        }
+        json_data[function] = d
+        with open("data.json", "w") as f:
+            json.dump(json_data, f)
+
+    for function, data in json_data.items():
+        if function in multi_gf_functions:
+            dataframes.append(pd.DataFrame(data))
+
+    df_all = pd.concat(dataframes)
+    df_pivot = df_all.pivot_table(index="dataset", columns="function", values="time")
     df_plot = df_pivot.loc[:, :].plot.line(figsize=(10, 7))
 
     print(df_pivot)
